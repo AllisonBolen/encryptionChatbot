fb306ee65134b96aa93b96eade9af4accbb592dc
Allison Bolen <bolena@mail.gvsu.edu>
2018-03-15 19:27:38 -0400

    concurrent hash map with ip keys

--- tcpechoserver.java
+++ tcpechoserver.java
@@ -3,20 +3,29 @@ import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap; // https://stackoverflow.com/questions/2836267/concurrenthashmap-in-java
 
 class tcpechoserver{
     public static void main(String args[]){
 	Scanner scan = new Scanner(System.in);
+	// map to store all connected client ips and threads
+	ConcurrentHashMap<SocketAddress, Integer> clientMap = new ConcurrentHashMap<SocketAddress, Integer>();
 	try{
 	    System.out.println("Enter a port for the server to run on: ");
 	    int port = scan.nextInt();
 	    ServerSocketChannel c = ServerSocketChannel.open();
 	    c.bind(new InetSocketAddress(port));
+	    int count = 0;
 	    while(true){
-		SocketChannel sc = c.accept();
-		System.out.println("Client Connected: " + sc.getRemoteAddress());
-		TcpServerThread t = new TcpServerThread(sc);
-		t.start();
+			SocketChannel sc = c.accept();
+            if (!clientMap.containsKey(sc.getRemoteAddress())) {
+                clientMap.putIfAbsent(sc.getRemoteAddress(), count);
+            }
+			System.out.println("Client Connected: " + sc.getRemoteAddress());
+			TcpServerThread t = new TcpServerThread(sc);
+			t.start();
+			System.out.println(clientMap.toString());
+			count++;
 	    }
 	} catch(IOException e){
 	    System.out.println("Got an IO Exception");
